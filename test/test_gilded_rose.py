cc8baa68f5a4994f6885f9585a1d59807e2d3981
dpwdec <declankolakowski@gmail.com>
2020-06-18 17:07:59 +0100

    Created test using magic method mocks.

--- test/test_gilded_rose.py
+++ test/test_gilded_rose.py
@@ -1,14 +1,39 @@
 # -*- coding: utf-8 -*-
-import unittest
+from unittest import TestCase
+from unittest import mock
 
 from src.gilded_rose import Item, GildedRose
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+class GildedRoseTest(TestCase):
+
+    def test_item_quality_lowered_by_one(self):
+        pass
+        foo_item = mock.Mock()
+        foo_item.name = "foo"
+
+        number_mock = mock.MagicMock()
+
+        __mock_gt__ = mock.Mock()
+        __mock_gt__.return_value = True
+        
+        __mock_sub__ = mock.MagicMock()
+        __mock_sub__.return_value = number_mock
+
+        __mock_lt_ = mock.MagicMock()
+        __mock_lt_.return_value = False
+        
+        number_mock.__gt__ = __mock_gt__
+        number_mock.__lt__ = __mock_lt_
+        number_mock.__sub__ = __mock_sub__
+
+        foo_item.quality = number_mock
+        foo_item.sell_in = number_mock
+
+        items = [foo_item]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
 
-if __name__ == '__main__':
-    unittest.main()
+        foo_item.quality.__sub__.assert_called_with(1)
+
+
+    

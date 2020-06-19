5881555c766c794fd9daa569aadd2c3f9149dc86
dpwdec <declankolakowski@gmail.com>
2020-06-19 15:46:22 +0100

    refactored aged brie out of main block into item specific block.

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -4,40 +4,29 @@ class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
-
-    def update(self):
-        for item in self.items:
-            if item.name == "Age Brie":
-                pass
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                pass
-            if item.name == "Sulfuras, Hand of Ragnaros":
-                pass
-            else:
-                self._update_regular_quality(item)
-    
-    def _update_regular_quality(self, item):
-        item.quality -= 1
     
     def update_quality(self):
         for item in self.items:
             if item.name == "Aged Brie":
+                if item.quality < 50:
+                    item.quality = item.quality + 1
                 item.sell_in = item.sell_in - 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 item.sell_in = item.sell_in - 1
             elif item.name == "Sulfuras, Hand of Ragnaros":
                 pass
             else:
-                #pass
                 if item.quality > 0:
                     item.quality = item.quality - 1                
                 if item.sell_in < 0:
                     if item.quality > 0:
                         item.quality = item.quality - 1                
                 item.sell_in = item.sell_in - 1
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                pass
-            else:
+
+
+
+
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality = item.quality + 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
@@ -47,9 +36,7 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                pass
-                #item.sell_in = item.sell_in - 1
+
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":

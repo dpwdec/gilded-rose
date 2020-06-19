db10bccb942dde3a8ff80c6677f930e3558d52c6
dpwdec <declankolakowski@gmail.com>
2020-06-19 15:05:52 +0100

    Added and refactored backstage pass tests.

--- test/test_backstage_passes.py
+++ test/test_backstage_passes.py
@@ -3,18 +3,19 @@ from unittest import mock
 
 from src.gilded_rose import GildedRose
 
-class TestGildedRose(TestCase):
-    pass
+class TestBackstagePasses(TestCase):
 
-class TestBackstagePasses(TestGildedRose):
-    
     def setUp(self):
         self.backstage_pass = mock.Mock()
-        self.backstage_pass.quality = 10
-        self.backstage_pass.sell_in = 11
         self.backstage_pass.name = "Backstage passes to a TAFKAL80ETC concert"
         items = [self.backstage_pass]
         self.gilded_rose = GildedRose(items)
+
+class GreaterThanTenDays(TestBackstagePasses):
+    
+    def setUp(self):
+        super().setUp()
+        self.backstage_pass.sell_in = 11
 
     def test_update_item_quality(self):
         """
@@ -22,5 +23,42 @@ class TestBackstagePasses(TestGildedRose):
         each cycle
         when sell_in is great than 10
         """
+        self.backstage_pass.quality = 10
         self.gilded_rose.update_quality()
         self.assertEqual(self.backstage_pass.quality, 11)
+    
+    def test_update_item_quality_capped(self):
+        """
+        backstage pass item quality cannot go
+        above 50
+        """
+        self.backstage_pass.quality = 50
+        self.gilded_rose.update_quality()
+        self.assertEqual(self.backstage_pass.quality, 50)
+
+
+class BetweenTenAndSixDays(TestBackstagePasses):
+
+    def setUp(self):
+        super().setUp()
+        self.backstage_pass.sell_in = 9
+
+    def test_update_item_quality(self):
+        """
+        backstage pass quality increases by 1
+        each cycle
+        when sell_in is less than 10
+        and sell_in is greater than 5
+        """
+        self.backstage_pass.quality = 10
+        self.gilded_rose.update_quality()
+        self.assertEqual(self.backstage_pass.quality, 12)
+
+    def test_update_item_quality_capped(self):
+        """
+        backstage pass item quality cannot go
+        above 50
+        """
+        self.backstage_pass.quality = 50
+        self.gilded_rose.update_quality()
+        self.assertEqual(self.backstage_pass.quality, 50)

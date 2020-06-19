2ae3a4f48452de715f7e4237bc16e786b500555c
dpwdec <declankolakowski@gmail.com>
2020-06-19 15:59:34 +0100

    Added logic for _change_quality method to backstage passes.

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -14,16 +14,14 @@ class GildedRose(object):
                     self._change_quality(item, 1)
                 item.sell_in = item.sell_in - 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                if item.sell_in < 0:
-                    item.quality = item.quality - item.quality
+                if item.sell_in > 10:
+                    self._change_quality(item, 1)
+                elif item.sell_in <= 10 and item.sell_in > 5:
+                    self._change_quality(item, 2)
+                elif item.sell_in <= 5 and item.sell_in > -1:
+                    self._change_quality(item, 3)
+                else:
+                    self._change_quality(item, -item.quality)
                 item.sell_in = item.sell_in - 1
             elif item.name == "Sulfuras, Hand of Ragnaros":
                 pass

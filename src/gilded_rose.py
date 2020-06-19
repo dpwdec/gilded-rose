4366ebd9892112a5525fe521964d54ef1a9e6364
dpwdec <declankolakowski@gmail.com>
2020-06-19 15:53:05 +0100

    Refactored regular item block to use change quality method with quality controls.

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -17,25 +17,28 @@ class GildedRose(object):
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
                 if item.sell_in < 0:
                     item.quality = item.quality - item.quality
                 item.sell_in = item.sell_in - 1
             elif item.name == "Sulfuras, Hand of Ragnaros":
                 pass
             else:
-                if item.quality > 0:
-                    item.quality = item.quality - 1                
                 if item.sell_in < 0:
-                    if item.quality > 0:
-                        item.quality = item.quality - 1                
+                    self._change_quality(item, -2)
+                else:
+                    self._change_quality(item, -1)               
                 item.sell_in = item.sell_in - 1
+
+    def _change_quality(self, item, amount):
+        item.quality += amount
+        if item.quality > 50: item.quality = 50
+        if item.quality < 0: item.quality = 0
 
     def _reduce_quality(self, item):
         pass

d2daeeed5fd58a5eeb7bfc35dafc965fa8599284
dpwdec <declankolakowski@gmail.com>
2020-06-19 15:54:30 +0100

    Refactored agedbrie to use _change_quality method.

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -8,11 +8,10 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name == "Aged Brie":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
                 if item.sell_in < 0:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self._change_quality(item, 2)
+                else:
+                    self._change_quality(item, 1)
                 item.sell_in = item.sell_in - 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:

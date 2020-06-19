db9106c1c441b955a47ef65b7b6880a729c91115
dpwdec <declankolakowski@gmail.com>
2020-06-19 16:03:35 +0100

    Refactored sell_in changed to a single statement in update.

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -12,7 +12,6 @@ class GildedRose(object):
                     self._change_quality(item, 2)
                 else:
                     self._change_quality(item, 1)
-                item.sell_in = item.sell_in - 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.sell_in > 10:
                     self._change_quality(item, 1)
@@ -22,14 +21,13 @@ class GildedRose(object):
                     self._change_quality(item, 3)
                 else:
                     self._change_quality(item, -item.quality)
-                item.sell_in = item.sell_in - 1
-            elif item.name == "Sulfuras, Hand of Ragnaros":
-                pass
-            else:
+            elif item.name != "Sulfuras, Hand of Ragnaros":
                 if item.sell_in < 0:
                     self._change_quality(item, -2)
                 else:
                     self._change_quality(item, -1)               
+        
+            if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
 
     def _change_quality(self, item, amount):

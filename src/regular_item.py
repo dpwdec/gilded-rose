7ced2bd18e73516b16214a67824c169a7b1dfc42
dpwdec <declankolakowski@gmail.com>
2020-06-19 12:37:55 +0100

    Refactored RegularItem to use super classes increment_quality methods.

--- src/regular_item.py
+++ src/regular_item.py
@@ -3,8 +3,7 @@ from src.variable_item import VariableItem
 class RegularItem(VariableItem):
 
     def update_quality(self):
-        if self.quality <= 0: return
         if self.sell_in > 0:
-            self.quality -= 1
+            self.increment_quality(-1)
         else:
-            self.quality -= 2
+            self.increment_quality(-2)

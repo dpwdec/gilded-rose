47744431ac3d45d97ea20624d768313ec67fe967
dpwdec <declankolakowski@gmail.com>
2020-06-19 15:20:02 +0100

    Refactored regular item tests.

--- test/test_regular_item.py
+++ test/test_regular_item.py
@@ -3,20 +3,20 @@ from unittest import TestCase
 from unittest import mock
 
 from src.gilded_rose import GildedRose
-from src.item import Item
 
-class TestGildedRose(TestCase):
-    pass
-
-class TestRegularItemBeforeSellBy(TestGildedRose):
+class TestRegularItem(TestCase):
 
     def setUp(self):
         self.foo_item = mock.Mock()
-        self.foo_item.quality = 10
-        self.foo_item.sell_in = 10
         self.foo_item.name = "Foo"
         items = [self.foo_item]
         self.gilded_rose = GildedRose(items)
+
+class BeforeSellBy(TestRegularItem):
+
+    def setUp(self):
+        super().setUp()
+        self.foo_item.sell_in = 10
 
     def test_item_quality_lowered(self):
         """
@@ -24,6 +24,7 @@ class TestRegularItemBeforeSellBy(TestGildedRose):
         each update cycle
         when sell in is above 0
         """
+        self.foo_item.quality = 10
         self.gilded_rose.update_quality()
         self.assertEqual(self.foo_item.quality, 9)
 
@@ -32,18 +33,24 @@ class TestRegularItemBeforeSellBy(TestGildedRose):
         sell in drops by one 
         each update cycle
         """
+        self.foo_item.quality = 10
         self.gilded_rose.update_quality()
         self.assertEqual(self.foo_item.sell_in, 9)
 
-class TestRegularItemAfterSellBY(TestGildedRose):
+    def test_item_quality_floor_capped(self):
+        """
+        regular item quality cannot drop below zero 
+        """
+        self.foo_item.quality = 0
+        self.gilded_rose.update_quality()
+        self.assertEqual(self.foo_item.quality, 0)
+
+
+class AfterSellBy(TestRegularItem):
 
     def setUp(self):
-        self.foo_item = mock.Mock()
-        self.foo_item.quality = 10
+        super().setUp()
         self.foo_item.sell_in = 0
-        self.foo_item.name = "Foo"
-        items = [self.foo_item]
-        self.gilded_rose = GildedRose(items)
 
     def test_item_quality_lowered(self):
         """
@@ -51,22 +58,14 @@ class TestRegularItemAfterSellBY(TestGildedRose):
         each update cycle
         when sell in is above 0
         """
+        self.foo_item.quality = 10
         self.gilded_rose.update_quality()
         self.assertEqual(self.foo_item.quality, 8)
 
-class TestItemQualityAboveZero(TestGildedRose):
-
-    def setUp(self):
-        self.foo_item = mock.Mock()
+    def test_item_quality_floor_capped(self):
+        """
+        regular item quality cannot drop below zero 
+        """
         self.foo_item.quality = 0
-        self.foo_item.sell_in = 10
-        self.foo_item.name = "Foo"
-        items = [self.foo_item]
-        self.gilded_rose = GildedRose(items)
-
-    def test_item_quality_not_lower(self):
-        """
-        regular item qualit cannot drop below zero    
-        """
         self.gilded_rose.update_quality()
         self.assertEqual(self.foo_item.quality, 0)

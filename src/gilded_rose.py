c2e224e5fccbfa28fc761b4b3cd2c801fedd0380
dpwdec <declankolakowski@gmail.com>
2020-06-19 15:37:09 +0100

    extracted first part of regular item if loop

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -21,10 +21,22 @@ class GildedRose(object):
     
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name == "Aged Brie":
+                pass
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                pass
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                pass
+            else:
+                #pass
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1                
+                # if item.sell_in < 0:
+                #     if item.quality > 0:
+                #         item.quality = item.quality - 1                
+                # item.sell_in = item.sell_in - 1
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                pass
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1

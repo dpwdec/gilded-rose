06e9b2422e8652fbc93394eda9e7509bc929bcd7
dpwdec <declankolakowski@gmail.com>
2020-06-19 15:49:16 +0100

    Refactored backstage pass age logic into single block.

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -15,6 +15,15 @@ class GildedRose(object):
                         item.quality = item.quality + 1
                 item.sell_in = item.sell_in - 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        if item.sell_in < 11:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+                        if item.sell_in < 6:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
                 item.sell_in = item.sell_in - 1
             elif item.name == "Sulfuras, Hand of Ragnaros":
                 pass
@@ -30,22 +39,10 @@ class GildedRose(object):
 
 
             if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-
+                pass
             if item.sell_in < 0:
                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            pass
-                            #item.quality = item.quality - 1
+                    pass
                 else:
                     item.quality = item.quality - item.quality
 

dc66883b7bc860a6aa3a1fefd1f2a3dc29176c46
dpwdec <declankolakowski@gmail.com>
2020-06-19 12:55:35 +0100

    Added legendary item class and non variable sell in

---
+++ src/legendary_item.py
@@ -0,0 +1,7 @@
+from src.variable_item import VariableItem
+
+class LegendaryItem(VariableItem):
+    pass
+
+    def update_sell_in(self):
+        pass
---
+++ test/test_legendary_item.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from src.legendary_item import LegendaryItem
+
+class TestLegendaryItem(TestCase):
+
+    def test_update_sell_in(self):
+        """
+        LegendaryItem's sell_in never changes
+        """
+        legendary_item = LegendaryItem("Sulfuras, Hand of Ragnaros", 10, 10)
+        legendary_item.update_sell_in()
+        self.assertEqual(legendary_item.sell_in, 10)
